bf58dbd11224cbbd90964a577b68f1453d19e086
Denis Khalikov <dennis.khalikov@gmail.com>
2019-04-04 14:23:57 +0300

    [Vulkan] Start of implementing IR Printer.

--- tensorflow/compiler/xla/service/spirv_ir/ir_builder.h
+++ tensorflow/compiler/xla/service/spirv_ir/ir_builder.h
@@ -22,8 +22,11 @@ limitations under the License.
 #include <memory>
 #include <vector>
 // TODO: Fix this, when integrates with XLA.
-#include <iostream>
 #include <cstdint>
+#include <iostream>
+#include <sstream>
+#include <unordered_map>
+
 using uint32 = uint32_t;
 
 namespace xla {
@@ -61,8 +64,9 @@ class SPIRVContext {
   spv::Id unique_id{0};
 };
 
+// Simple singleton.
 SPIRVContext *GetSPIRVContext() {
-  static SPIRVContext *context = new SPIRVContext ();
+  static SPIRVContext *context = new SPIRVContext();
   return context;
 }
 
@@ -73,6 +77,8 @@ class Operand {
   Operand() : id_(0), is_id_(true) {}
   Operand(spv::Id id, bool is_id = true) : id_(id), is_id_(is_id) {}
   bool IsId() { return is_id_; }
+  spv::Id GetId() { return id_; }
+
  private:
   spv::Id id_;
   bool is_id_;
@@ -81,7 +87,7 @@ class Operand {
 class IRVisitor {
  public:
   IRVisitor() {}
-  virtual ~IRVisitor() = 0;
+  virtual ~IRVisitor() {}
   virtual void Visit(Instruction *instruction) = 0;
 };
 
@@ -96,25 +102,20 @@ class Instruction {
         type_id_(type_id),
         operands_(std::move(operands)) {}
 
-  Instruction(spv::Op op_code, spv::Id result_id, spv::Id type_id,
-              spv::StorageClass storage_class)
-      : op_code_(op_code),
-        result_id_(result_id),
-        type_id_(type_id),
-        storage_class_(storage_class) {}
-
   Instruction(const Instruction &other) = delete;
   Instruction(Instruction &&other) = delete;
   Instruction &operator=(const Instruction &other) = delete;
   Instruction &operator=(Instruction &&other) = delete;
   ~Instruction() {}
 
-  void SetBasicBlock(BasicBlock *parent_bb) {}
   void AddOperand(spv::Id operand_id, bool is_id = true) {
     operands_.push_back(Operand(operand_id, is_id));
   }
 
   spv::Op GetOpCode() { return op_code_; }
+  spv::Id GetResultId() { return result_id_; }
+  spv::Id GetTypeId() { return type_id_; }
+  std::vector<Operand> GetOperands() { return operands_; }
   void Accept(IRVisitor *visitor) { visitor->Visit(this); }
   size_t WordCount() { return word_count_; }
 
@@ -124,59 +125,90 @@ class Instruction {
   spv::Id type_id_{0};
   std::vector<Operand> operands_;
   size_t word_count_{0};
-  BasicBlock *parent_bb_{nullptr};
-  spv::StorageClass storage_class_{spv::StorageClass::StorageClassUniform};
 };
 
 class IRPrinter : public IRVisitor {
+ public:
   IRPrinter() {}
-  ~IRPrinter () {}
+  ~IRPrinter() {}
+
   void Visit(Instruction *instruction) {
     switch (instruction->GetOpCode()) {
       case spv::Op::OpVariable:
-        std::cout << "OpVariable " << std::endl;
+        ProcessInstruction(instruction, "OpVariable");
         break;
       case spv::Op::OpLoad:
-        std::cout << "OpLoad " << std::endl;
+        ProcessInstruction(instruction, "OpLoad");
         break;
       case spv::Op::OpStore:
-        std::cout << "OpStore " << std::endl;
+        ProcessInstruction(instruction, "OpStore");
         break;
       case spv::Op::OpBranch:
-        std::cout << "OpBranch " << std::endl;
+        ProcessInstruction(instruction, "OpBranch");
         break;
       case spv::Op::OpLabel:
-        std::cout << "OpLabel " << std::endl;
+        ProcessInstruction(instruction, "OpLabel");
         break;
       case spv::Op::OpBranchConditional:
-        std::cout << "OpBranchConditional  " << std::endl;
+        ProcessInstruction(instruction, "OpBranchConditional");
         break;
       case spv::Op::OpAccessChain:
-        std::cout << "OpAccessChain " << std::endl;
+        ProcessInstruction(instruction, "OpAccessChain");
         break;
       case spv::Op::OpInBoundsAccessChain:
-        std::cout << "OpInBoundsAccessChain  " << std::endl;
+        ProcessInstruction(instruction, "OpInBoundsAccessChain");
         break;
       case spv::Op::OpPhi:
-        std::cout << "OpPhi " << std::endl;
+        ProcessInstruction(instruction, "OpPhi");
+        break;
+      case spv::Op::OpConstant:
+        ProcessInstruction(instruction, "OpConstant");
         break;
       default:
-        std::cout << "not implemented " << std::endl;
         break;
     }
   }
+
+  void ProcessInstruction(Instruction *instruction, std::string op_code) {
+    if (instruction->GetResultId()) {
+      stream_ << ident_ << instruction->GetResultId() << white_space_ << assign_
+              << white_space_;
+    }
+    stream_ << op_code << white_space_;
+    if (instruction->GetTypeId()) {
+      stream_ << ident_ << instruction->GetTypeId() << white_space_;
+    }
+    std::vector<Operand> operands = instruction->GetOperands();
+    for (size_t i = 0; i < operands.size(); ++i) {
+      if (operands[i].IsId()) {
+        stream_ << ident_ << operands[i].GetId() << white_space_;
+      } else {
+        // TODO: Lookup for literal
+      }
+    }
+
+    stream_ << new_line_;
+  }
+
+ private:
+  std::stringstream stream_;
+  char ident_ = '%';
+  char white_space_ = ' ';
+  char assign_ = '=';
+  char new_line_ = '\n';
 };
 
 class BasicBlock {
  public:
-  BasicBlock(std::string name, Function *function, SPIRVContext *context)
-      : name_(std::move(name)), function_(function), ctx_(context) {
+  BasicBlock(std::string name)
+      : name_(std::move(name)) {
     // Each basic blocks starts from unique label.
-    spv::Id id = ctx_->GetUniqueId();
+    spv::Id id = GetSPIRVContext()->GetUniqueId();
     Instruction *instruction = new Instruction(spv::Op::OpLabel, 0, 0, {id});
     instructions_.push_back(instruction);
     label_id_ = id;
   }
+  ~BasicBlock() {}
 
   void AddInstruction(Instruction *instruction) {
     instructions_.push_back(instruction);
@@ -190,15 +222,13 @@ class BasicBlock {
 
   spv::Id Label() { return label_id_; }
 
-  ~BasicBlock() {}
+  std::vector<Instruction *> &GetInstructions() { return instructions_; }
 
  private:
   std::vector<Instruction *> instructions_;
   std::vector<BasicBlock *> predeccessors_;
   std::vector<BasicBlock *> successors_;
   std::string name_;
-  Function *function_;
-  SPIRVContext *ctx_;
   spv::Id label_id_{0};
 };
 
@@ -207,27 +237,89 @@ class Function {
  public:
   Function(std::string function_name)
       : function_name_(std::move(function_name)) {}
-  void AddBasicBlock(BasicBlock *bb) { cfg_.push_back(bb); }
+  void AddBasicBlock(BasicBlock *bb) { basic_blocks_.push_back(bb); }
+
+  void AddEntryBlock(BasicBlock *bb) {
+    entry_block_ = bb;
+    basic_blocks_.push_back(bb);
+  }
+
+  void AddRetBlock(BasicBlock *bb) {
+    ret_block_ = bb;
+    basic_blocks_.push_back(bb);
+  }
+
+  std::vector<BasicBlock *> &GetBasicBlocks() { return basic_blocks_; }
+
  private:
-  std::vector<BasicBlock *> cfg_;
+  std::vector<BasicBlock *> basic_blocks_;
   std::string function_name_;
-  BasicBlock *entry_point;
+  Instruction *entry_point_;
+  BasicBlock *entry_block_;
+  BasicBlock *ret_block_;
 };
 
 // Module contains functions, each function contains entry point.
 class Module {
+ public:
   Module(std::string module_name) : module_name_(std::move(module_name)) {}
-  Function *CreateFunction(std::string name) { return nullptr; }
+
+  Module (const Module &other) = delete;
+  Module &operator=(const Module &other) = delete;
+  Module(Module &&other) = delete;
+  Module &operator=(Module &&other) = delete;
+
+  ~Module() {
+    // clear all
+  }
+
+  void Accept(IRVisitor *visitor) {
+    // TODO:
+    // Visit Decorator table at first.
+    // Visit Custom types table at second.
+
+    for (auto &table_instance : functions_) {
+      // Think about reordering of the basic blocks.
+      for (BasicBlock *bb : table_instance.second->GetBasicBlocks()) {
+        for (Instruction *instruction : bb->GetInstructions()) {
+          visitor->Visit(instruction);
+        }
+      }
+    }
+  }
+
+  void InitBasicTypesAndConstants() {}
+  // TODO: Init all basic types by default.
+
+  // TODO: Implement it.
+  spv::Id GetOrCreateCustomType(std::string type_name) { return 0; }
+  spv::Id GetOrCreateConstant(std::string value) { return 0; }
+  spv::Id GetOrCreateVariable(std::string var_name) { return 0; }
+  Function *GetOrCreateFunction(std::string name,
+                                std::string function_control = "None") {
+    /*
+      spv::Id id = ctx_->GetUniqueId();
+      spv::Id literal_id = ctx_->GetUniqueId();
+      literal_pool_id.insert({literal_id, function_control});
+      Instruction *instruction = new Instruction(spv::Op::OpFunction, id, type,
+                                                 {literal_id, function_type});
+      instert_point_->
+      */
+    return nullptr;
+  }
+
  private:
-  std::vector<Function *> functions_;
+  std::unordered_map<std::string, Function *> functions_;
   std::string module_name_;
+  std::unordered_map<std::string, Instruction *> user_types_table_;
+  std::unordered_map<std::string, Instruction *> user_variales_table_;
 };
 
 // Class which builds the IR.
 class IRBuilder {
  public:
-  IRBuilder(BasicBlock *insert_point, SPIRVContext *context)
-      : insert_point_(insert_point), ctx_(context) {}
+  IRBuilder(BasicBlock *insert_point, Module *module)
+      : insert_point_(insert_point), module_(module) {}
   IRBuilder(const IRBuilder &other) = delete;
   IRBuilder(IRBuilder &&other) = delete;
   IRBuilder &operator=(const IRBuilder &other) = delete;
@@ -242,7 +334,7 @@ class IRBuilder {
 
   // %id = OpLoad %type %ptr MemAccesstype
   spv::Id CreateLoad(spv::Op type, spv::Id ptr) {
-    spv::Id id = ctx_->GetUniqueId();
+    spv::Id id = GetSPIRVContext()->GetUniqueId();
     Instruction *instruction =
         new Instruction(spv::Op::OpLoad, id, type, {ptr});
     insert_point_->AddInstruction(instruction);
@@ -290,7 +382,7 @@ class IRBuilder {
   // %id = OpAccessChain %ptr_type %ptr {%offsets}
   spv::Id CreateAccesssChain(spv::Op op_code, spv::Id ptr_type, spv::Id ptr,
                              const std::vector<spv::Id> &offsets) {
-    spv::Id id = ctx_->GetUniqueId();
+    spv::Id id = GetSPIRVContext()->GetUniqueId();
     std::vector<Operand> operands{ptr_type, ptr};
     for (size_t i = 0; i < offsets.size(); ++i) {
       operands.push_back(offsets[i]);
@@ -302,7 +394,7 @@ class IRBuilder {
 
   // %id = OpBinOp %type %lhs %rhs
   spv::Id CreateBinOp(spv::Op op_code, spv::Id type, spv::Id lhs, spv::Id rhs) {
-    spv::Id id = ctx_->GetUniqueId();
+    spv::Id id = GetSPIRVContext()->GetUniqueId();
     Instruction *instruction = new Instruction(op_code, id, type, {lhs, rhs});
     insert_point_->AddInstruction(instruction);
     return id;
@@ -312,7 +404,7 @@ class IRBuilder {
   spv::Id CreatePhi(
       spv::Id type,
       const std::vector<std::pair<spv::Id, BasicBlock *>> &phi_values) {
-    spv::Id id = ctx_->GetUniqueId();
+    spv::Id id = GetSPIRVContext()->GetUniqueId();
     std::vector<Operand> operands;
 
     for (size_t i = 0; i < phi_values.size(); ++i) {
@@ -328,26 +420,33 @@ class IRBuilder {
     return id;
   }
 
-  // Could takes a literals.
-  // Figure out how to implement it.
-  spv::Id CreateType(spv::Op kind) { return 0; }
-  spv::Id CreatePointerType(spv::Id type) { return 0; }
-
   // %id = OpVariable %ptr StorageClass
-  spv::Id CreateVariable(spv::Id type, spv::StorageClass storage_class) {
-    spv::Id id = ctx_->GetUniqueId();
+  // The storage class defines, the "scope" of the variable.
+  spv::Id CreateVariable(spv::Id type, std::string storage_class) {
+    spv::Id id = GetSPIRVContext()->GetUniqueId();
+    spv::Id literal_id = GetSPIRVContext()->GetUniqueId();
+    literal_pool_.insert({literal_id, std::move(storage_class)});
     Instruction *instruction =
-        new Instruction(spv::Op::OpVariable, id, type, storage_class);
+        new Instruction(spv::Op::OpVariable, id, type, {literal_id});
     insert_point_->AddInstruction(instruction);
     return id;
   }
 
-  spv::Id CreateConstant(spv::Op type, int value) { return 0; }
-  spv::Id CreateFunction() { return 0; }
+  // %id = OpConstant %type %literal_value
+  spv::Id CreateConstant(spv::Id type, std::string value) {
+    spv::Id id = GetSPIRVContext()->GetUniqueId();
+    spv::Id literal_id = GetSPIRVContext()->GetUniqueId();
+    literal_pool_.insert({literal_id, std::move(value)});
+    Instruction *instruction =
+        new Instruction(spv::Op::OpConstant, id, type, {literal_id});
+    insert_point_->AddInstruction(instruction);
+    return id;
+  }
 
  private:
   BasicBlock *insert_point_;
-  SPIRVContext *ctx_;
+  Module *module_;
+  std::unordered_map<spv::Id, std::string> literal_pool_;
 };
 }  // namespace spriv
 }  // namespace xla
--- tensorflow/compiler/xla/service/spirv_ir/ir_builder_test.cc
+++ tensorflow/compiler/xla/service/spirv_ir/ir_builder_test.cc
@@ -3,13 +3,28 @@
 int main() {
   using namespace xla;
   using namespace spirv;
-  IRPrinter printer;
-  Function *function = new Function("shader");
-  BasicBlock *entry = new BasicBlock("entry", function,
-                                     GetSPIRVContext());  // new BasicBlock();
-  IRBuilder builder(entry, GetSPIRVContext());
-  spv::Id some_type = 21;
-  spv::Id var =
-      builder.CreateVariable(some_type, spv::StorageClass::StorageClassUniform);
+  IRPrinter *printer = new IRPrinter();
+  Module *module = new Module("test");
+
+  spv::Id void_id = module->GetOrCreateCustomType("void");
+  spv::Id int_id = module->GetOrCreateCustomType("int");
+
+  Function *function = module->GetOrCreateFunction("test_function", "None");
+
+  BasicBlock *entry = new BasicBlock("entry");
+  BasicBlock *next_block = new BasicBlock("next");
+  BasicBlock *ret = new BasicBlock("ret");
+  
+  function->AddEntryBlock(entry);
+  function->AddRetBlock(ret);
+
+  IRBuilder *builder = new IRBuilder(entry, module);
+  builder->SetInsertPoint(entry);
+  builder->CreateBr(next_block);
+  builder->SetInsertPoint(next_block);
+  builder->CreateBr(ret);
+  // entry -> next -> ret
+  //
+  module->Accept(printer);
   return 0;
 }
